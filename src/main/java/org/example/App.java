df59d49a6ae7f2b3d4779c3598b2167e1f3531f3
liding <539561425@qq.com>
2021-06-26 23:51:15 +0800

    fix: 修改代码

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,18 @@
 package org.example;
 
-/**
- * Hello world!
- */
 public class App {
+    /**
+     * bootstrap:
+     * parse command line
+     * set up environment
+     * kick off main logic
+     * @param args
+     */
     public static void main(String[] args) {
+        printHelloWorld();
+    }
+
+    private static void printHelloWorld() {
         System.out.println("Hello World!");
     }
 }
